42a19d72a5a6b3fbdec8d9fc052bad563d8b964d
ErosSense1 <107019348+ErosSense1@users.noreply.github.com>
2022-07-16 17:16:10 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import {msg, texto, clean} from './functions.js'
+import {msg, texto, clean} from 'https://raw.githubusercontent.com/ErosSense1/chat-bot/main/functions.js'
 
 let but = document.getElementById("button");
 texto.onclick = clean
